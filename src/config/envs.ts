83bf1da4337214c6f21b3032e011d7ebcc1e98a5
JGCoder96 <jgcoder96@gmail.com>
2025-07-14 15:22:55 -0400

    arreglos en las variables de entorno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,5 +7,4 @@ export const envs = {
   TELEGRAM_BOT_TOKEN: get('TELEGRAM_BOT_TOKEN').required().asString(),
   URL: get('URL').required().asString(),
   PORT: get('PORT').default('8080').asPortNumber(),
-  HOST: get('HOST').default('localhost').asString(),
 };
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { server } from './server';
 const main = () => {
   const app = server();
   const port = envs.PORT || 8080;
-  const host = envs.HOST || 'localhost';
-  const httpServer = app.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  const httpServer = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
   });
   httpServer.timeout = 300000;
 };
